b983bb58d66a3a1016952444ce3915bae097d2ed
Sahil Arote <arotesahil2@gmail.com>
2025-11-21 20:36:06 +0530

    its ready

--- LU.js
+++ LU.js
@@ -1,1 +1,34 @@
-console.log("hellow");
+ let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+    function showTodos() {
+      const list = document.getElementById("todoList");
+      list.innerHTML = "";
+
+      todos.forEach((t, i) => {
+        list.innerHTML += `
+          <li>
+            ${t}
+            <button onclick="removeTask(${i})">X</button>
+          </li>
+        `;
+      });
+    }
+
+    function addTask() {
+      const input = document.getElementById("todoInput");
+      if (input.value.trim() === "") return;
+
+      todos.push(input.value);
+      input.value = "";
+      localStorage.setItem("todos", JSON.stringify(todos));
+      showTodos();
+    }
+
+    function removeTask(i) {
+      todos.splice(i, 1);
+      localStorage.setItem("todos", JSON.stringify(todos));
+      showTodos();
+    }
+
+
+    showTodos();
